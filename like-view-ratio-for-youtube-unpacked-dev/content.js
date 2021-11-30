1e9bc94021cb16456908c83194cb2470253db75d
Thomas Berghuis <thomas.berghuis@gmail.com>
2021-11-30 13:22:19 +1100

    .

--- like-view-ratio-for-youtube-unpacked-dev/content.js
+++ like-view-ratio-for-youtube-unpacked-dev/content.js
@@ -21,43 +21,29 @@ function createButton() {
 }
 
 function getLikes() {
-  console.log("test fn");
-  var text = document.querySelector("#top-level-buttons-computed #text");
-  console.log("text", text);
+  var likesText = document.querySelector("#top-level-buttons-computed #text");
+  var likes = likesText.ariaLabel.replace(/\D/g, "");
 
-  // var label = text.ariaLabel;
-  var likes = text.ariaLabel.replace(/\D/g, "");
-  console.log("~ likes", likes);
-
-  // var views = document.querySelector("ytd-video-view-count-renderer .view-count");
   var views = document.querySelector(".view-count");
-
   views = views.textContent.replace(/\D/g, "");
-  console.log("~ views", views);
 
   var ratio = (likes / views) * 100;
   ratio = Number.parseFloat(ratio).toPrecision(3);
-  console.log("~ ratio", ratio);
 
   insertRatio(ratio);
 }
 
 function insertRatio(ratio) {
-  // #info #top-level-buttons-computed.ytd-menu-renderer
-
   var container = document.querySelector(
     "#info #top-level-buttons-computed.ytd-menu-renderer"
   );
 
   var ratioDisplay = document.createElement("div");
-
   ratioDisplay.innerHTML = `L/V ${ratio}&#37;`;
   ratioDisplay.style.fontSize = "14px";
   ratioDisplay.style.fontWeight = 500;
   ratioDisplay.style.height = "40px";
   ratioDisplay.style.lineHeight = "40px";
-
-  // eElement.insertBefore(newFirstElement, eElement.firstChild);
 
   container.insertBefore(ratioDisplay, container.firstChild);
 }
